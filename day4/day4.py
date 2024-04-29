e6003e564566aabc10d0f459df6af470f99af3b1
Martin <martin.evenbom@gmail.com>
2024-04-29 23:19:16 +0200

    day4 part 1 solved

--- day4/day4.py
+++ day4/day4.py
@@ -2,18 +2,31 @@ from timeit import default_timer as timer
 
 def read_input(input):
     with open(f'{input}.txt', 'r') as f:
-        data = f.read().splitlines()
+        data = f.read().strip()
     return data
 
-def sum(data):
+score = 0
 
+input = read_input('input')
+
+lines = input.split("\n")
+
+for line in lines:
+    first, second = line.split("|")
+    id, card = first.split(":")
+    card_nums = [int(x) for x in card.split()]
+    second_nums = [int(x) for x in second.split()]
+    val = len(set(card_nums) & set(second_nums))
+    if val > 0:
+        score +=  2**(val - 1)
 
 if __name__ == "__main__":
     start = timer()
-    print("The result to 1 is: {}".format(sum))
+    print("The result to 1 is: {}".format(score))
     end = timer()
+'''
     print(end - start)
-'''    start = timer()
+    start = timer()
     print("The result to 2 is: {}".format(sum2))
     end = timer()
     print(end - start)'''
