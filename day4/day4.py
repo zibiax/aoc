c3345104877dc6b0243869be2762913694a3d757
Martin <martin.evenbom@gmail.com>
2024-04-30 16:37:47 +0200

    day4 solved

--- day4/day4.py
+++ day4/day4.py
@@ -1,5 +1,5 @@
 from timeit import default_timer as timer
-
+from collections import defaultdict
 def read_input(input):
     with open(f'{input}.txt', 'r') as f:
         data = f.read().strip()
@@ -11,6 +11,8 @@ input = read_input('input')
 
 lines = input.split("\n")
 
+D = defaultdict(int)
+
 for line in lines:
     first, second = line.split("|")
     id, card = first.split(":")
@@ -20,13 +22,26 @@ for line in lines:
     if val > 0:
         score +=  2**(val - 1)
 
+for i, line in enumerate(lines):
+    D[i] += 1
+    first, second = line.split("|")
+    id, card = first.split(":")
+    card_nums = [int(x) for x in card.split()]
+    second_nums = [int(x) for x in second.split()]
+    val = len(set(card_nums) & set(second_nums))
+    
+    for j in range(val):
+        D[i + 1 + j] += D[i]
+
+    score2 = sum(D.values())
+   
+
 if __name__ == "__main__":
     start = timer()
     print("The result to 1 is: {}".format(score))
     end = timer()
-'''
     print(end - start)
     start = timer()
-    print("The result to 2 is: {}".format(sum2))
+    print("The result to 2 is: {}".format(score2))
     end = timer()
-    print(end - start)'''
+    print(end - start)
