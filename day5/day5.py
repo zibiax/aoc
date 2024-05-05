19361a898ee879274adfb787b8d70ebe395e489b
Martin <martin.evenbom@gmail.com>
2024-05-05 22:17:56 +0200

    first part of day5

--- day5/day5.py
+++ day5/day5.py
@@ -1,5 +1,7 @@
 from timeit import default_timer as timer
 from collections import defaultdict
+
+
 def read_input(input):
     with open(f'{input}.txt', 'r') as f:
         data = f.read().strip()
@@ -7,6 +9,29 @@ def read_input(input):
 
 score = 0
 
+data = read_input('input')
+seed, *blocks = data.split("\n\n")
+
+seeds = list(map(int, seed.split(":")[1].split()))
+
+for block in blocks:
+    ranges = []
+    for line in block.splitlines()[1:]:
+        ranges.append(list(map(int, line.split())))
+
+    new = []
+
+    for x in seeds:
+        for a, b, c in ranges:
+            if b <= x < b + c:
+                new.append(x - b + a)
+                break
+        
+        else:
+            new.append(x)
+    seeds = new
+
+score = min(seeds)
 
 if __name__ == "__main__":
     start = timer()
