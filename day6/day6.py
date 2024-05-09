60ec49836827a53459df23f1811e21f61e7e5e9a
Martin <martin.evenbom@gmail.com>
2024-05-09 16:50:14 +0200

    day6 part 1 and 2 done

--- day6/day6.py
+++ day6/day6.py
@@ -14,5 +14,32 @@ data = read_input('input')
 time, distance = data.split("\n")
 time = time.split(":")[1].split()
 distance = distance.split(":")[1].split()
-print(time)
-print(distance)
+
+
+def calculate_ways(time, distance):
+    counter = 0
+    for i in range(1, time + 1):
+        speed = i
+        rt = time - i
+        td = rt * speed
+        if td >= distance:
+            counter += 1
+    return counter
+
+#counter = calculate_ways(int(time), int(distance))
+start = timer()
+total_counter = 1
+for i in range(4):
+    counter = calculate_ways(int(time[i]), int(distance[i]))
+    total_counter *= counter
+total_counter -= 1
+print(total_counter)
+end = timer()
+print(end - start)
+time, distance = data.split("\n")
+time = time.split(":")[1].replace(" ", "")
+distance = distance.split(":")[1].replace(" ", "")
+start = timer()
+print(calculate_ways(int(time), int(distance)))
+end = timer()
+print(end - start)
